4a91aa4841bafd78798184a9f80b0184bc69c341
RicardoStoklosa <stoklosa.ricardo@gmai.com>
2019-10-26 14:16:03 -0300

    Arrumando a bagunça

--- node/app.js
+++ node/app.js
@@ -1,11 +1,11 @@
 const express    = require("express");
 const app        = express();
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const routes     = require("./config/routes");
 
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 app.use(routes);
 
